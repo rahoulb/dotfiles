3ea6fd9b3f6042c604d1ce0d516b012e52145624
Rahoul Baruah <rahoulb@echodek.co>
2026-03-10 11:06:48 +0000

    Removed unimportant aliases

--- zsh/oh-my-zsh-custom/aliases.zsh
+++ zsh/oh-my-zsh-custom/aliases.zsh
@@ -8,20 +8,10 @@ if [[ $(uname) == "Darwin" ]]; then
   alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
   alias tmx="tmux -CC new -A -s rahoulb"
 
-  function devconnect () {
-    ssh -i $HOME/.ssh/rahoulb rahoulb@xstation -t -- ssh $1.devpod
-  }
 else
   alias tmx="tmux new -A -s rahoulb"
   # Use native nvim instead of flatpak (flatpak breaks terminal state)
   alias nvim="/opt/nvim-linux-x86_64/bin/nvim"
 
-  function devconnect () {
-    ssh -i $HOME/.ssh/rahoulb rahoulb@xbeast -t -- ssh $1.devpod
-  }
 fi
-
-function devssh () {
-  ssh $1.devpod
-}
 
