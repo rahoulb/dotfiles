06024c88f42473e0eca49d642abf594638221087
Rahoul Baruah <rahoulb@echodek.co>
2026-03-24 09:43:13 +0000

    Updated aliases for devcontainers

--- zsh/oh-my-zsh-custom/aliases.zsh
+++ zsh/oh-my-zsh-custom/aliases.zsh
@@ -1,8 +1,8 @@
 # Aliases
-alias dccup='devcontainer up --workspace-folder .'
-alias dccsh='devcontainer exec --workspace-folder . bash -l'
-alias dccdown='docker compose -f .devcontainer/compose.yaml down'
-alias dcc='docker compose'
+alias devup="devcontainer up --workspace-folder . && devcontainer exec --workspace-folder . bash -l"
+alias devsh='devcontainer exec --workspace-folder . bash -l'
+alias devdown='docker compose -f .devcontainer/compose.yaml down'
+alias devexec="devcontainer exec --workspace-folder . bash -lc"
 alias control='printf "\e]1;%s\a" "Control" && ssh -i $HOME/.ssh/rahoulb control@control.echodek.co'
 alias python=python3
 alias cher='openclaw'
@@ -10,11 +10,9 @@ alias cher='openclaw'
 if [[ $(uname) == "Darwin" ]]; then
   alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
   alias tmx="tmux -CC new -A -s rahoulb"
-
 else
   alias tmx="tmux new -A -s rahoulb"
   # Use native nvim instead of flatpak (flatpak breaks terminal state)
   alias nvim="/opt/nvim-linux-x86_64/bin/nvim"
-
 fi
 
