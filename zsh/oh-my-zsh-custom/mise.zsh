6ab5b1c279de3a19417636f2cfbe547c827c4a54
Rahoul Baruah <rahoulb@echodek.co>
2026-04-11 11:16:55 +0100

    Updated for office machine

--- zsh/oh-my-zsh-custom/mise.zsh
+++ zsh/oh-my-zsh-custom/mise.zsh
@@ -8,6 +8,6 @@ if command -v mise &>/dev/null; then
   eval "$(mise activate zsh)"
 elif [ -x "$HOME/.local/bin/mise" ]; then
   eval "$($HOME/.local/bin/mise activate zsh)"
-elif [ -x "/usr/bin/mise" ]; then
-  eval "$(/usr/bin/mise activate zsh)"
+elif [ -x "/usr/local/bin/mise" ]; then
+  eval "$(/usr/local/bin/mise activate zsh)"
 fi
