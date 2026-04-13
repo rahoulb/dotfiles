0dd0391eed919f1e6e9a6d97e7abd7d18d8ee298
Rahoul Baruah <rahoulb@echodek.co>
2026-04-13 15:47:15 +0100

    Updated for linux

--- zsh/oh-my-zsh-custom/aliases.zsh
+++ zsh/oh-my-zsh-custom/aliases.zsh
@@ -5,14 +5,12 @@ alias devdown='docker compose -f .devcontainer/compose.yaml down'
 alias devexec="devcontainer exec --workspace-folder . bash -lc"
 alias control='printf "\e]1;%s\a" "Control" && ssh -i $HOME/.ssh/rahoulb control@control.echodek.co'
 alias python=python3
-alias cher='openclaw'
+alias cher=openclaw
 
 if [[ $(uname) == "Darwin" ]]; then
   alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
   alias tmx="tmux -CC new -A -s rahoulb"
 else
   alias tmx="tmux new -A -s rahoulb"
-  # Use native nvim instead of flatpak (flatpak breaks terminal state)
-  alias nvim="/opt/nvim-linux-x86_64/bin/nvim"
 fi
 
