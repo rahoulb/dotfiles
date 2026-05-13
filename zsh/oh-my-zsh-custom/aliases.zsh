48c89b521524af4b7220140e3077b5b4a84d224f
Rahoul Baruah <rahoulb@echodek.co>
2026-05-13 09:36:10 +0100

    Added aliases for connecting to the agents via SSH and tmux

--- zsh/oh-my-zsh-custom/aliases.zsh
+++ zsh/oh-my-zsh-custom/aliases.zsh
@@ -5,6 +5,9 @@ alias devdown='docker compose -f .devcontainer/compose.yaml down'
 alias devexec="devcontainer exec --workspace-folder . bash -lc"
 alias control='printf "\e]1;%s\a" "Control" && ssh -i $HOME/.ssh/rahoulb control@control.echodek.co'
 alias python=python3
+alias cher="ssh -i $HOME/.ssh/rahoulb rahoulb@pismo-beach 'tmux -CC attach -t Cher'"
+alias dionne="ssh -i $HOME/.ssh/rahoulb rahoulb@pismo-beach 'tmux -CC attach -t Dionne'"
+alias tai="ssh -i $HOME/.ssh/rahoulb rahoulb@tai 'tmux -CC attach -t Tai'"
 
 if [[ $(uname) == "Darwin" ]]; then
   alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
